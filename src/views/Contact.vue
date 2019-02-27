98fb88b79b5ecd374063e6910d86c8b90a681608
Vrijraj Singh <vrijraj2396@gmail.com>
2019-02-27 18:08:35 +0530

    Some Bugs Fixed

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an Contact page</h1>
-  </div>
+  <v-content class="pa-0">
+
+    <v-container fluid class="mt-4 mb-0">
+      <v-layout wrap align-center justify-center row fill-height class="my-0">
+        <v-flex xs12 md10 class="mb-0">
+            <p class="google-font mb-0 mt-0" style="font-size:200%">Contact {{ChapterDetails.ChapterName}}</p>
+            <v-divider></v-divider>
+
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid class="">
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10 class="">
+          <aboutGDGChapter/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+
+
+  </v-content>
+  
 </template>
+
+<script>
+import ChapterDetails from '@/assets/data/chapterDetails.json'
+import aboutGDGChapter from '@/components/about/aboutGDGChapter'
+
+  export default {
+    components: {
+      aboutGDGChapter,
+    },
+    data() {
+      return {
+        ChapterDetails: ChapterDetails
+      }
+    },
+  }
+</script>
