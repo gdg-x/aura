ff93756154e9aa260ffc87adc5aa7d616dc7d488
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-11 19:56:34 +0530

    URL name updated

--- src/config/key.js
+++ src/config/key.js
@@ -1,5 +1,5 @@
 export const MeetupAPI = {
-  urlname: "CHAPTER_URL_NAME" // Example GDG-Jalandhar  CHAPTER_URL_NAME
+  urlname: "GDG-Jalandhar" // Example GDG-Jalandhar  CHAPTER_URL_NAME
 };
 
 // For urlname: extract the urlname from meetup chapter link-> https://meetup.com/GDG-Jalandhar
