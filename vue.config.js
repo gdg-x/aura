fada43eaa574637d265dc32da3d1dc798cd24346
Rossano D'Angelo <dangelo.rossano.94@gmail.com>
2019-08-17 17:48:46 +0100

    Fix the webapp name

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   pwa: {
-    name: 'GDG Web App',
-    workboxPluginMode: 'InjectManifest',
-    themeColor: '#4A90E2',
+    name: "GDG Galway",
+    workboxPluginMode: "InjectManifest",
+    themeColor: "#4A90E2",
     workboxOptions: {
-      swSrc: './src/sw.js',
-      swDest: 'service-worker.js',
-    },
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js"
+    }
   }
-}
+};
