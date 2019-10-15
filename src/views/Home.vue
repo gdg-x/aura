1e4875449a9ae9b52edaf65f2eb4f86eb5df344f
Vrijraj Singh <vrijraj2396@gmail.com>
2019-10-15 17:01:18 +0530

    home.vue u

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,75 @@
 <template>
-  <v-container fluid>
-    <homeStartScreen />
-    <h1>lo</h1>
-  </v-container>
+  <v-content class="px-0">
+    <v-container fluid class="px-0">
+      <v-row justify="center" align="center">
+        <v-col md="12" lg="10" xs="12" class="py-0">
+          <homeStartScreen />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="pa-0 py-2" >
+      <v-row justify="center" align="center">
+        <v-col md="12" lg="10" xs="12" class="py-0 " :class="this.$vuetify.theme.dark == true?'darkModeContainer':'lightModeContainer'">
+          <whatwedo />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="pa-0 py-2" >
+      <v-row justify="center" align="center">
+        <v-col md="12" lg="10" xs="12" class="py-0" >
+          <aboutCommunity />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="pa-0 py-2" >
+      <v-row justify="center" align="center">
+        <v-col md="12" lg="10" xs="12" class="py-0">
+          <events />
+        </v-col>
+      </v-row>
+    </v-container>
+
+
+    <v-container fluid class="pa-0 py-0" >
+      <v-row justify="center" align="center" class="py-4" :class="this.$vuetify.theme.dark == true?'grey darken-4':'grey lighten-4'">
+        <v-col md="12" lg="10" xs="12" class="py-0">
+          <featureEvents />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="pa-0 py-2" >
+      <v-row justify="center" align="center">
+        <v-col md="12" lg="10" xs="12" class="py-0">
+          <partners />
+        </v-col>
+      </v-row>
+    </v-container>
+
+  </v-content>
 </template>
 
 <script>
-import homeStartScreen from '../components/home/homeStartScreen';
+import homeStartScreen from '../components/home/homeScreen';
+import whatwedo from '../components/home/whatwedo';
+import aboutCommunity from '../components/home/aboutCommunity';
+import events from '../components/home/events';
+import featureEvents from '../components/home/featureEvents';
+import partners from '../components/common/partners'
 
 export default {
   components: {
     homeStartScreen,
+    whatwedo,
+    aboutCommunity,
+    events,
+    featureEvents,
+    partners
   },
 };
 </script>
+
+
