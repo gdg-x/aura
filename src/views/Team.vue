142dbbb9fa9f980dd29eded9a852c5088b74e905
Vrijraj Singh <vrijraj2396@gmail.com>
2019-11-09 23:39:07 +0530

    team c u

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content class="px-0 mx-0 pt-1">
     <v-container fluid class="px-0 pt-5 mt-5" >
-      <v-row justify="center" align="center" class="py-3" :class="this.$vuetify.theme.dark == true?'black':'primary'" >
+      <v-row justify="center" align="center" class="py-3" :class="this.$vuetify.theme.dark == true?'black':'grey lighten-4'" >
         <v-col md="12" lg="10" xs="12" class="pt-3 bottom-space"  >
           <teamHeader />
         </v-col>
