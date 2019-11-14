319bf35c765d87017fb4f9d468e14d1629fe7615
Snickdx <snickdx@gmail.com>
2019-11-14 08:31:36 -0400

    merge conflicts

--- src/config/key.js
+++ src/config/key.js
@@ -1,5 +1,5 @@
 export const MeetupAPI = {
+  apiKey: '285329611a5c156320621d65766234a',
   urlname: "GDG-Port-of-Spain" // Example GDG-Jalandhar  CHAPTER_URL_NAME
 };
 
-// For urlname: extract the urlname from meetup chapter link-> https://meetup.com/GDG-Jalandhar
