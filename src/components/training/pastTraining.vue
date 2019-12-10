d5513d1fc402a1e556100781fe41a5ad024754cc
Lipee Vora <lvora@mitre.org>
2019-12-10 10:39:11 -0500

    Cleaned up some unneccessary files

--- src/components/training/pastTraining.vue
+++ src/components/training/pastTraining.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-col cols="12" md="12" lg="12" sm="12" class>
-        <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Directory of past Trainings</p>
+        <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Directory of Past Trainings</p>
         <p class="google-font mt-0 mb-0" style="font-size:95%" >Trainings are listed in chronological order by date.</p>
       </v-col>
       
@@ -16,8 +16,8 @@
            <v-list-item three-line> 
               <v-list-item-avatar
                 tile
-                size="50"
-                color="grey"
+                size="30"
+                color="#327cdb"
               ></v-list-item-avatar>
               
               <v-list-item-content>
@@ -30,6 +30,11 @@
         <!-- </v-sheet> -->
       </v-row>
     </v-container>
+    
+    <v-col cols="12" md="12" lg="12" sm="12" class>
+        <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Directory of Upcoming Trainings</p>
+        <p class="google-font mt-0 mb-0" style="font-size:95%" >Trainings are listed in chronological order by date.</p>
+      </v-col>
       
       <!-- <v-row v-if="showLoader">
           <v-col md="4" lg="4" sm="6" cols="12" class="pa-0" v-for="i in 6" :key="i">
