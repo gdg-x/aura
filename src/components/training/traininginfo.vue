c089345231f7e9ebf50e521bb657d38ada86890b
Will Dower <wdower@mitre.org>
2019-12-13 12:31:19 -0500

    - built out Training page to include Resources and Video sections - fixed the dialog boxes to actually show the data from the card you clicked
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/training/traininginfo.vue
+++ src/components/training/traininginfo.vue
@@ -1,115 +1,142 @@
 <template>
   <v-container fluid>
-    <v-col cols="12" md="12" lg="12" sm="12" class>
-      <p
-        class="google-font mb-0"
-        style="font-size:150%;color: #1a73e8;"
-      >Directory of Upcoming Trainings</p>
-      <p
-        class="google-font mt-0 mb-0"
-        style="font-size:95%"
-      >Trainings are listed in chronological order by date.</p>
-    </v-col>
-
+    <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Training Resources</p>
     <br />
-
-    <v-container fluid>
-      <v-row>
+    <v-list v-for="item in resources.items" :key="item">
+      <v-list-item-title style="font-size:125% ma-2">
+        <b>{{item.name}}</b>
+      </v-list-item-title>
+      <v-list-item class="ma-2">{{item.desc}}</v-list-item>
+      <v-list-item v-for="entry in item.values" :key="entry">
+        <div>
+          <li>
+            <span><a :href="entry.link">{{entry.name}}</a><span v-show="entry.desc"> -- {{entry.desc}}</span></span>
+          </li>
+        </div>
+      </v-list-item>
+    </v-list>
+    <br />
+    <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Videos</p>
+    <v-row>
+      <v-col v-for="video in resources.videos" :key="video" cols="auto">
+        <iframe
+          :src="video.link"
+          :width="$vuetify.breakpoint.smAndDown ? 280 : 560"
+          :height="$vuetify.breakpoint.smAndDown ? 150 : 300"
+        ></iframe>
+      </v-col>
+    </v-row>
+    <br />
+    <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Upcoming Training Sessions</p>
+    <p
+      class="google-font mt-0 mb-0"
+      style="font-size:95%"
+    >Trainings are listed in chronological order by date.</p>
+    <br />
+    <v-row>
+      <v-col
+        md="12"
+        lg="10"
+        sm="12"
+        class="pa-2"
+        v-for="item in trainingData.upcomingTraining.introTraining"
+        :key="item"
+      >
+        <v-dialog v-model="dialog">
+          <template v-slot:activator="{ on }">
+            <p class="google-font ma-0 mt-0" style="font-size:120%;">{{item.name}}</p>
+          </template>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <br />
+    <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Past Training Sessions</p>
+    <p
+      class="google-font mt-0 mb-0"
+      style="font-size:95%"
+    >Trainings are listed in chronological order by date.</p>
+    <p class="google-font mt-0 mb-0" style="font-size:95%">
+      <b>Note:</b> Video recordings of these trainings will be made available once they are edited for presentability.
+    </p>
+    <v-row justify="center" :class="color_mode" class="ma-2">
+      <v-slide-group class="pa-2 ma-2" show-arrows>
         <v-col
-          md="12"
-          lg="10"
-          sm="6"
-          cols="6"
+          md="3"
+          lg="3"
+          sm="3"
+          cols="3"
           class="pa-2"
-          v-for="item in trainingData.upcomingTraining.introTraining"
+          v-for="item in trainingData.pastTraining.introTraining"
           :key="item"
         >
-          <v-dialog v-model="dialog">
+          <v-dialog v-model="dialog" :dialogData="item" width="700">
             <template v-slot:activator="{ on }">
-              <p class="google-font ma-0 mt-0" style="font-size:120%;">{{item.name }}</p>
+              <div
+                v-on="on"
+                style="cursor: pointer;width: 275px; height: 150px; white-space: initial;"
+                :class="$vuetify.theme.dark == true?'darkModeCard':'lightModeCard'"
+                class="pa-2"
+                @click="passItemData(item)"
+              >
+                <p class="google-font mt-1 mb-0 break-word" style="font-size:90%">{{ item.date }}</p>
+                <p class="google-font ma-0 mt-0 break-word" style="font-size:120%;">{{ item.name }}</p>
+                <p class="mb-0 mt-2 google-font" style="color:#1a73e8">See More</p>
+              </div>
             </template>
+
+            <v-card color v-show="dialog" :data="dialogData">
+              <v-card-title
+                class="px-5 py-5 google-font break-word"
+                :class="{'x-small': $vuetify.breakpoint.mdAndDown}"
+                style="background-position:right bottom;"
+              >{{dialogData.name}}</v-card-title>
+
+              <v-card-text class="pa-5">
+                <p class="google-font mt-3 mb-0 break-word" style="font-size:110%">
+                  <b>Name:</b>
+                  {{dialogData.name}}
+                </p>
+                <p class="google-font mt-1 mb-0" style="font-size:110%">
+                  <b>Date:</b>
+                  {{dialogData.date}}
+                </p>
+                <v-spacer class="ma-2" />
+                <a
+                  v-if="dialogData.link != ''"
+                  :href="dialogData.link"
+                  target="_blank"
+                >Link to Recording</a>
+                <p v-else>
+                  <b>Link to Recording --</b>
+                  <span style="color:red">Video still being edited! Please check back later.</span>
+                </p>
+              </v-card-text>
+
+              <v-divider></v-divider>
+            </v-card>
           </v-dialog>
         </v-col>
-      </v-row>
-    </v-container>
-
-    <br />
-
-    <v-col cols="12" md="12" lg="12" sm="12" class>
-      <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;">Directory of Past Trainings</p>
-      <p
-        class="google-font mt-0 mb-0"
-        style="font-size:95%"
-      >Trainings are listed in chronological order by date.</p>
-    </v-col>
-
-    <v-container fluid>
-      <v-row justify="center" :class="color_mode">
-        <v-slide-group class="pa-2 ma-2" show-arrows>
-          <v-col
-            md="3"
-            lg="3"
-            sm="3"
-            cols="3"
-            class="pa-2"
-            v-for="item in trainingData.pastTraining.introTraining"
-            :key="item"
-          >
-            <v-dialog v-model="dialog" width="700">
-              <template v-slot:activator="{ on }">
-                <div
-                  v-on="on"
-                  style="cursor: pointer;width: 275px; height: 150px; white-space: initial;"
-                  :class="$vuetify.theme.dark == true?'darkModeCard':'lightModeCard'"
-                  class="pa-2"
-                >
-                  <p class="google-font mt-1 mb-0 break-word" style="font-size:90%">{{ item.date }}</p>
-                  <p
-                    class="google-font ma-0 mt-0 break-word"
-                    style="font-size:120%;"
-                  >{{ item.name }}</p>
-                  <p class="mb-0 mt-2 google-font" style="color:#1a73e8">See More</p>
-                </div>
-              </template>
-
-              <v-card color v-if="dialog">
-                <v-card-title
-                  class="px-5 py-5 google-font"
-                  :class="{'x-small': $vuetify.breakpoint.mdAndDown}"
-                  style="background-position:right bottom;"
-                >{{item.name}}</v-card-title>
-
-                <v-card-text class="pa-5">
-                  <p class="google-font mt-3 mb-0" style="font-size:110%">
-                    <b>Name:</b>
-                    {{item.name}}
-                  </p>
-                  <p class="google-font mt-1 mb-0" style="font-size:110%">
-                    <b>Date:</b>
-                    {{item.date}}
-                  </p>
-                </v-card-text>
-
-                <v-divider></v-divider>
-              </v-card>
-            </v-dialog>
-          </v-col>
-        </v-slide-group>
-      </v-row>
-    </v-container>
+      </v-slide-group>
+    </v-row>
   </v-container>
 </template>
 
-        
 <script>
 import communityData from "@/assets/data/communityData.json";
 import trainingData from "@/assets/data/trainingData.json";
+import resources from "@/assets/data/resources.json";
 export default {
   name: "App",
   data: () => ({
     communityData: communityData,
     trainingData: trainingData,
-    dialog: false
+    resources: resources,
+    dialog: false,
+    dialogData: {
+      name: "",
+      date: "",
+      link: ""
+    }
   }),
   computed: {
     color_mode() {
@@ -122,6 +149,14 @@ export default {
       } else {
         return "";
       }
+    }
+  },
+  methods: {
+    make_linkable(str) {
+      return str.replace(/\s+/g, "-").toLowerCase();
+    },
+    passItemData(item) {
+      this.dialogData = item;
     }
   }
 };
