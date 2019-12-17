453a602de4d5c48227528c2aa72b78fe33d3ef63
Lipee Vora <lvora@mitre.org>
2019-12-17 15:32:39 -0500

    fixed weird spacing issue in gs

--- src/components/gettingstarted/gsInfo.vue
+++ src/components/gettingstarted/gsInfo.vue
@@ -1,31 +1,29 @@
 <template>
   <v-container fluid>
     <p class="google-font mb-0" style="font-size:150%;color: #1a73e8;"></p>
-    <br />
-
-    <v-list two-line>
-      <template v-for="item in resources.items">
-        <v-list-item :key="item">
-          <v-list-item-content>
-            <!-- <v-list-item-title style="font-size:150% ma-2">
+      <v-list two-line>
+        <template v-for="item in resources.items">
+          <v-list-item :key="item">
+            <v-list-item-content>
+              <!-- <v-list-item-title style="font-size:150% ma-2">
               <b>{{item.name}}</b>
-            </v-list-item-title> -->
-            <v-list-item class="ma-2">{{item.desc}}</v-list-item>
-            <v-list-item v-for="entry in item.values" :key="entry">
-              <div>
-                <li>
-                  <span>
-                    <a :href="entry.link">{{entry.name}}</a>
-                    <span v-show="entry.desc">-- {{entry.desc}}</span>
-                  </span>
-                </li>
-              </div>
-            </v-list-item>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
-    <br />
+              </v-list-item-title>-->
+              <v-list-item class="ma-2">{{item.desc}}</v-list-item>
+              <v-list-item v-for="entry in item.values" :key="entry">
+                <div>
+                  <li>
+                    <span>
+                      <a :href="entry.link">{{entry.name}}</a>
+                      <span v-show="entry.desc">-- {{entry.desc}}</span>
+                    </span>
+                  </li>
+                </div>
+              </v-list-item>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+      <br />
   </v-container>
 </template>
 
