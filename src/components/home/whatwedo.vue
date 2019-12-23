19cd83879522699008c0d68082cad65904eec40d
Will Dower <wdower@mitre.org>
2019-12-23 17:02:07 -0500

    - demo refactor of the whatWeDo view - refactor is intended to give people a little more background on what our main products are right from the get-go on the homepage - MITRE SAF ==> INSPEC for testing, HEIMDALL for analyzing the tests, is what we want in people's heads
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/home/whatwedo.vue
+++ src/components/home/whatwedo.vue
@@ -22,7 +22,7 @@
             >
               <v-card outlined v-bind:href="item.link" target="_blank" class="pa-2" flat>
                 <v-icon large style="font-size:300%">{{ item.icon }}</v-icon>
-                <p class="google-font mt-2 title" >{{ item.name }}</p>
+                <p class="google-font mt-2 title">{{ item.name }}</p>
                 <p class="google-font" style="font-size:95%">{{ item.desc }}</p>
               </v-card>
             </v-col>
@@ -37,23 +37,83 @@
         <p
           class="google-font mt-0"
           style="font-size:95%"
-        >Discover MITRE's tool chain for enabling secure development.</p>
+        >MITRE has developed a workflow for generating good security data - and acting on it.</p>
       </v-col>
       <v-col cols="12" md="9" lg="9" sm="12">
         <v-container fluid>
-          <v-row>
+          <v-row justify="center">
             <v-col
               v-for="(item, i) in whatwedotools"
               :key="i"
-              md="3"
-              lg="3"
+              md="6"
+              lg="6"
               sm="6"
               :cols="$vuetify.breakpoint.smAndDown ? '6' : '3'"
               class="d-flex"
             >
-              <v-card outlined v-bind:href="item.link" target="_blank" flat class="d-flex flex-column pa-2">
-                <p class="google-font text-center title" style="font-size:130%">{{ make_readable(item.name) }}</p>
-                <p class="text-left ma-2 google-font text-center" style="font-size:95%">{{ item.desc }}</p>
+              <v-card outlined :href="item.link" target="_blank" flat class="pa-2">
+                <v-row justify="center" align="center">
+                  <v-col cols="auto">
+                <v-img
+                  v-show="item.svg"
+                  :src="require('@/assets/img/svg/' + item.svg + '.svg')"
+                  align="center"
+                  style="max-width: 50px; max-height: 50px;"
+                  class="ma-2"
+                  contain
+                />
+                  </v-col>
+                </v-row>
+                <p
+                  class="google-font text-center title"
+                  style="font-size:140%"
+                >{{ make_readable(item.name) }}</p>
+                <p
+                  class="text-left ma-2 google-font text-center"
+                  style="font-size:95%"
+                >{{ item.desc }}</p>
+
+                <v-card-text
+                  class="google-font text-center font-weight-bold align-bottom ma-2"
+                  style="font-size:120%"
+                >{{item.summary}}</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+    <v-divider />
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="3" lg="3" sm="12" class="text-left pa-2">
+        <p class="google-font mb-2 tool-header">Tools We Use</p>
+        <p
+          class="google-font mt-0"
+          style="font-size:95%"
+        >MITRE has developed a library of resources to support secure development.</p>
+      </v-col>
+      <v-col cols="12" md="9" lg="9" sm="12">
+        <v-container fluid>
+          <v-row justify="center">
+            <v-col
+              v-for="(item, i) in whatwedolinks"
+              :key="i"
+              md="6"
+              lg="3"
+              sm="3"
+              :cols="$vuetify.breakpoint.smAndDown ? '6' : '3'"
+              class="d-flex"
+            >
+              <v-card outlined :href="item.link" target="_blank" flat class="pa-2">
+                <p
+                  class="google-font text-center title"
+                  style="font-size:140%"
+                >{{ make_readable(item.name) }}</p>
+                <p
+                  class="text-left ma-2 google-font text-center"
+                  style="font-size:95%"
+                >{{ item.desc }}</p>
+
               </v-card>
             </v-col>
           </v-row>
@@ -69,16 +129,17 @@ export default {
   name: "App",
   data: () => ({
     whatwedocats: whatwedodata.whatWeDoCategories,
-    whatwedotools: whatwedodata.whatWeDoTools
+    whatwedotools: whatwedodata.whatWeDoTools,
+    whatwedolinks: whatwedodata.whatWeDoLinks
   }),
   filters: {
     truncate(value, bp) {
-      if (bp.smAndDown && (value.length > 75)) {
-          return value.substring(0, 72) + "..."
+      if (bp.smAndDown && value.length > 75) {
+        return value.substring(0, 72) + "...";
       } else if (value.length > 150) {
-        return value.substring(0, 147) + "..."
+        return value.substring(0, 147) + "...";
       } else {
-        return value
+        return value;
       }
     }
   },
@@ -92,7 +153,7 @@ export default {
     },
     make_readable(str) {
       return str.replace(/_/, " ");
-    },
+    }
   }
 };
 </script>
