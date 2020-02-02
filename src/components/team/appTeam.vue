0269828cdda6e470b39e3ed3ae50a1e2e176d5f8
Nasir Hussain <nasirhussainm14@gmail.com>
2020-02-02 20:45:20 +0500

    Web & App Added

--- src/components/team/appTeam.vue
+++ src/components/team/appTeam.vue
@@ -3,7 +3,7 @@
 
         <v-layout wrap align-start justify-start row fill-height class="mt-0 mb-0">
             <v-flex xs12 class="mb-3">
-                <p class="google-font" style="font-size:130%;">Our Team Members</p>
+                <p class="google-font" style="font-size:130%;">Our App Team Members</p>
                 <v-divider></v-divider>
             </v-flex>
             <v-flex xs12 md3 lg3 v-for="(item,i) in TeamDetails" :key="i">
@@ -54,7 +54,7 @@
 </template>
 
 <script>
-import TeamDetails from '@/assets/data/organizingTeam.json'
+import TeamDetails from '@/assets/data/appTeam.json'
 export default {
     data() {
         return {
