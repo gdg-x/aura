a8570b3cf89e7877985f1a418ab93a7cf4d43296
asadkhalid305 <asadkhalid305@gmail.com>
2020-02-04 01:48:27 +0500

    removed redundant code

--- src/components/common/Toolbar.vue
+++ src/components/common/Toolbar.vue
@@ -26,45 +26,6 @@
       flat
       @click="signin"
     >Signin</v-btn>
-    <!-- 
-    <v-menu app v-else v-model="menu" :close-on-content-click="false" :nudge-width="200" offset-x>
-      <template v-slot:activator="{ on }">
-        <v-btn dark icon v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-list>
-          <v-list-item>
-            <v-list-item-avatar>
-              <img :src="user.data.photoURL" alt="profile-photo" />
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title>{{user.data.displayName}}</v-list-item-title>
-              <v-list-item-subtitle>{{user.data.email}}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list>
-          <v-list-item>
-            <v-list-item-action>
-              <v-btn
-                class="ml-0 google-font hidden-sm-and-down"
-                style="text-transform: capitalize;"
-                flat
-                @click="logout"
-              >Logout</v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-menu>-->
-
     <Menu v-else :userData="user.data" @userLogout="logout" />
   </v-toolbar>
 </template>
