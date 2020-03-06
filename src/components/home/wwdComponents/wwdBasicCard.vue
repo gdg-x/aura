b274e923fd701da1009066b6d89d967c52cba3b9
Will Dower <wdower@mitre.org>
2020-03-06 18:31:55 -0500

    added app, documentation, and github links to the tools
    Signed-off-by: Will Dower <wdower@mitre.org>

--- src/components/home/wwdComponents/wwdBasicCard.vue
+++ src/components/home/wwdComponents/wwdBasicCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card outlined :href="item.link" :target='item.link[0] == "/" ? "" : "_blank"'  class="pa-2 ma-2" flat>
+  <v-card outlined class="pa-2 ma-2 d-flex flex-column" :to="item.link" flat>
     <!--<toolBlock v-if="item.tools" :tools="item.tools" />-->
     <v-icon v-if="item.icon" large style="font-size:300%">{{ item.icon }}</v-icon>
     <img
@@ -9,12 +9,44 @@
       dark
       style="max-width: 32px; max-height: 32px;"
     />
-    <p class="google-font mt-2 title align-bottom">{{ make_readable(item.name) }}</p>
-    <p class="google-font" style="font-size:95%;">{{ item.desc }}</p>
+    <v-card-title class="google-font mt-2 title align-bottom justify-center break-word">{{ make_readable(item.name) }}</v-card-title>
+    <v-card-text class="google-font">{{ item.desc }}</v-card-text>
+    <v-spacer> </v-spacer>
+    <v-card-actions>
+      <v-container fluid>
+      <v-row justify="center">
+          <v-btn color="primary" v-if="item.app_link" class="d-none d-sm-inline" :href="item.app_link" :target='item.app_link[0] == "/" ? "" : "_blank"' text>Demo</v-btn>
+          <v-btn color="primary" v-if="item.doc_link" class="d-none d-sm-inline " :href="item.doc_link" :target='item.doc_link[0] == "/" ? "" : "_blank"' text>Docs</v-btn>
+          <v-btn color="primary" v-if="item.github_link" class="d-none d-sm-inline " :href="item.github_link" :target='item.github_link[0] == "/" ? "" : "_blank"' text>GitHub</v-btn>
+          <a v-if="item.app_link" :href="item.app_link" target="_blank">
+            <img
+              :src="require('@/assets/img/svg/' + item.app_svg + '.svg')"
+              class="svg-style d-flex d-sm-none ma-2"
+            />
+          </a>
+          <a v-if="item.doc_link" :href="item.doc_link" target="_blank">
+            <img
+              :src="require('@/assets/img/svg/desc.svg')"
+              class="svg-style d-flex d-sm-none ma-2"
+            />
+          </a>
+          <a v-if="item.github_link" :href="item.github_link" target="_blank">
+            <img
+              :src="require('@/assets/img/svg/github.svg')"
+              class="svg-style d-flex d-sm-none ma-2"
+            />
+          </a>
+      </v-row>
+      </v-container>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+
+// see https://stackoverflow.com/questions/58556447/is-there-a-card-footer-card-deck-feature-on-vuetify
+// for details on arranging card components
+
 //import toolBlock from "@/components/home/wwdComponents/toolBlock.vue";
 export default {
   props: {
@@ -30,3 +62,22 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.card-outer {
+  position: relative;
+  padding-bottom: 10px;
+}
+.card-actions {
+  position: absolute;
+  bottom: 0;
+}
+.flexcard {
+  display: flex;
+  flex-direction: column;
+}
+.svg-style {
+  max-width: 24px;
+  max-height: 24px;
+}
+</style>
