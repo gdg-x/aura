11a0e6e4e8a7565224508f721c0205819d3c2d24
Vrijraj Singh <vrijraj2396@gmail.com>
2020-04-03 23:39:27 +0530

    First Commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
