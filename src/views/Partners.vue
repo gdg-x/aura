1fec8f8096c169593aa7b7af2809a1e211bb4552
Vrijraj Singh <vrijraj2396@gmail.com>
2020-04-08 23:56:03 +0530

    Partner page added

---
+++ src/views/Partners.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-content class="px-0 mx-0 pt-1">
+    <v-container fluid class="px-0 pt-5 mt-5" >
+      <v-row justify="center" align="center" class="py-3" :class="this.$vuetify.theme.dark == true?'black':'grey lighten-4'" >
+        <v-col md="12" lg="10" xs="12" class="pt-3 bottom-space"  >
+          <partners />
+        </v-col>
+      </v-row>
+    </v-container>
+
+  </v-content>
+</template>
+
+<script>
+import partners from '@/components/common/Partners';
+export default {
+  components: {
+    partners
+  },
+};
+</script>
