82a5172c44d60ef32bc42a1a1b20ead90f39ca99
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-04-10 22:56:47 +0530

    Custom service worker added

---
+++ src/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+        return;
+    }
+
+    switch (e.data) {
+        case 'skipWaiting':
+            self.skipWaiting();
+            break;
+        default:
+            // NOOP
+            break;
+    }
+});
+
+workbox.core.setCacheNameDetails({
+    prefix: "cache"
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
