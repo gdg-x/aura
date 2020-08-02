a316cd53b4ec49cd612c93d5976ca17d28284eae
Pablo Bautista <pablo@pixela.com.gt>
2020-08-01 19:24:17 -0600

    Deployment and init config

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   productionSourceMap: false,
   pwa: {
-    name: 'Aura Main',
+    name: 'GDG Xela',
     workboxPluginMode: 'InjectManifest',
     themeColor: '#4A90E2',
     msTileColor: '#4A90E2',
